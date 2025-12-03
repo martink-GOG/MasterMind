63304f28ae7b15fa169bcf4effc17d8e74bd2c6e
martink-GOG <mkersten2004@gmail.com>
2025-12-03 12:13:39 +0100

    random code and arrays added

--- src/masterMind/MasterMind.java
+++ src/masterMind/MasterMind.java
@@ -1,6 +1,7 @@
 package masterMind;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class MasterMind
 {
@@ -9,116 +10,93 @@ public class MasterMind
     {
 	// TODO Auto-generated method stub
 	Scanner sc = new Scanner(System.in);
-	
+	Random num= new Random();
 //code maker
 	String goedenKleurfoutenPlekPrint = "0";
 	String goedenKleurEnPlekPrint = "x";
 
 //code vak
-	System.out.println("code maker kiest een cijfer uit 1 t/m 6");
-	int codeVak1 = sc.nextInt();
-	System.out.println("code maker kiest een cijfer uit 1 t/m 6");
-	int codeVak2 = sc.nextInt();
-	System.out.println("code maker kiest een cijfer uit 1 t/m 6");
-	int codeVak3 = sc.nextInt();
-	System.out.println("code maker kiest een cijfer uit 1 t/m 6");
-	int codeVak4 = sc.nextInt();
 	
+
+	int[] codeVak = new int[4];
+	codeVak[0] = num.nextInt(6)+1;
+	codeVak[1] = num.nextInt(6)+1;
+	codeVak[2] = num.nextInt(6)+1;
+	codeVak[3] = num.nextInt(6)+1;
+	System.out.println("eerste poging   =" + codeVak[0] + codeVak[1] + codeVak[2] + codeVak[3]);
 	int levens = 10;
 	do {//start loop
 	    // 1st poging
+	    int[] poging = new int[4];
+	        System.out.println("code kraker kiest een cijfer uit 1 t/m 6");
+		poging[0] = sc.nextInt();
 		System.out.println("code kraker kiest een cijfer uit 1 t/m 6");
-		int poging1_1 = sc.nextInt();
+		poging[1] = sc.nextInt();
 		System.out.println("code kraker kiest een cijfer uit 1 t/m 6");
-		int poging1_2 = sc.nextInt();
+		poging[2] = sc.nextInt();
 		System.out.println("code kraker kiest een cijfer uit 1 t/m 6");
-		int poging1_3 = sc.nextInt();
-		System.out.println("code kraker kiest een cijfer uit 1 t/m 6");
-		int poging1_4 = sc.nextInt();
+		poging[3] = sc.nextInt();
 
 		System.out.println("x is goed ");
 		System.out.println("0 is verkeerde plek ");
 		System.out.println("* zit er niet in");
 		// controle 1
-		String controle1_1 = "*";
-		String controle1_2 = "*";
-		String controle1_3 = "*";
-		String controle1_4 = "*";
+	String[] controle = new String[4];
+		controle[0] = "*";
+		controle[1] = "*";
+		 controle[2] = "*";
+		controle[3] = "*";
 	    // poging1_1 contole check
-		boolean eersteControle = poging1_1 == codeVak1;
+		boolean eersteControle = poging[0] == codeVak[0];
 		if (eersteControle)
 		{
-		    controle1_1 = goedenKleurEnPlekPrint;
+		    controle[0] = goedenKleurEnPlekPrint;
 		} else
 		{
-		    if (poging1_1 == codeVak2 || poging1_1 == codeVak3 || poging1_1 == codeVak4)
+		    if (poging[0] == codeVak[1] || poging[0] == codeVak[2] || poging[0] == codeVak[3])
 		    {
-			controle1_1 = goedenKleurfoutenPlekPrint;
+			controle[0] = goedenKleurfoutenPlekPrint;
 		    } 
 		}
 		// poging1_2 contole check
-		boolean tweedenControle = poging1_2 == codeVak2;
+		boolean tweedenControle = poging[1] == codeVak[1];
 		if (tweedenControle)
 		{
-		    controle1_2 = goedenKleurEnPlekPrint;
+		    controle[1] = goedenKleurEnPlekPrint;
 		} else
 		{
-		    if (poging1_2 == codeVak1 || poging1_2 == codeVak3 || poging1_2 == codeVak4)
+		    if (poging[1] == codeVak[0] || poging[1] == codeVak[2] || poging[1] == codeVak[3])
 		    {
-			controle1_2 = goedenKleurfoutenPlekPrint;
+			controle[1] = goedenKleurfoutenPlekPrint;
 		    }
 		}
 		// poging1_3 contole check
-		boolean derdenControle =poging1_3 == codeVak3;
+		boolean derdenControle =poging[2] == codeVak[2];
 		if (derdenControle)
 		{
-		    controle1_3 = goedenKleurEnPlekPrint;
+		    controle[2] = goedenKleurEnPlekPrint;
 		} else
 		{
-		    if (poging1_3 == codeVak2)
+		    if (poging[2] == codeVak[1] || poging[2] == codeVak[0] || poging[2] == codeVak[3])
 		    {
-			controle1_3 = goedenKleurfoutenPlekPrint;
-		    } else
-		    {
-			if (poging1_3 == codeVak1)
-			{
-			    controle1_3 = goedenKleurfoutenPlekPrint;
-			} else
-			{
-			    if (poging1_3 == codeVak4)
-			    {
-				controle1_3 = goedenKleurfoutenPlekPrint;
-			    }
-			}
-		    }
+			controle[2] = goedenKleurfoutenPlekPrint;
+		    } 
 		}
 		// poging1_4 contole check
-		boolean vierdenControle =poging1_4 == codeVak4;
+		boolean vierdenControle =poging[3] == codeVak[3];
 		if (vierdenControle)
 		{
-		    controle1_4 = goedenKleurEnPlekPrint;
+		    controle[3] = goedenKleurEnPlekPrint;
 		} else
 		{
-		    if (poging1_4 == codeVak2)
+		    if (poging[3] == codeVak[1] || poging[3] == codeVak[2] || poging[3] == codeVak[0])
 		    {
-			controle1_4 = goedenKleurfoutenPlekPrint;
-		    } else
-		    {
-			if (poging1_4 == codeVak3)
-			{
-			    controle1_4 = goedenKleurfoutenPlekPrint;
-			} else
-			{
-			    if (poging1_4 == codeVak1)
-			    {
-				controle1_4 = goedenKleurfoutenPlekPrint;
-			    }
-			}
-		    }
+			controle[3] = goedenKleurfoutenPlekPrint;
+		    } 
 		}
 		//poging1 feedback
-		System.out.println("eerste poging   =" + poging1_1 + poging1_2 + poging1_3 + poging1_4);
-		System.out.println("controle poging =" + controle1_1 + controle1_2 + controle1_3 + controle1_4);
+		System.out.println("eerste poging   =" + poging[0] + poging[1] + poging[2] + poging[3]);
+		System.out.println("controle poging =" + controle[0] + controle[1] + controle[2] + controle[3]);
 		//winst controle
 		if (eersteControle && tweedenControle && derdenControle && vierdenControle)
 		{
