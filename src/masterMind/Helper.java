d4069d72d0591c28cc5f9da8fce59070e7951704
martink-GOG <mkersten2004@gmail.com>
2026-01-08 12:39:35 +0100

    helper class
    heb dingen over gezet van master mind naar helper class en weer terug aan geroepen

--- src/masterMind/Helper.java
+++ src/masterMind/Helper.java
@@ -1,6 +1,93 @@
 package masterMind;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Helper extends MasterMind
 {
 
+    private String goedenKleurfoutenPlekPrint = "0";
+    private String goedenKleurEnPlekPrint = "x";
+    public int codenGrooten = 4;
+    Random num = new Random();
+    private int[] codeVak = new int [codenGrooten];
+    public int levens = 10;
+    Scanner sc = new Scanner(System.in);
+    private int[] poging = new int[codenGrooten];
+    private boolean[] mainControle = new boolean[codenGrooten];
+    private String[] controle = new String[codenGrooten];
+//code vak
+
+    public int[] createCode()
+    {
+	for (int idx = 0; idx < codeVak.length; idx = idx + 1)
+	{
+	    codeVak[idx] = num.nextInt(6) + 1;
+	}
+	return codeVak;
+    }
+    public int [] pogingSpeler() {
+	
+	    for (int idx = 0; idx < poging.length; idx = idx + 1)
+	    {
+		System.out.println("code kraker kiest een cijfer uit 1 t/m 6");
+		poging[idx] = sc.nextInt();
+	    }
+	    return poging;
+    }
+    public String[] controlenCheck() {
+	 
+	    for (int idx = 0; idx < controle.length; idx = idx + 1)
+	    {
+		controle[idx] = "*";
+	    }
+
+	    
+	    for (int idx = 0; idx < controle.length; idx = idx + 1)
+	    {
+		mainControle[idx] = false;
+	    }
+	    // 1st contole checks
+	    for (int idx = 0; idx < poging.length; idx = idx + 1)
+	    {
+		if (poging[idx] == codeVak[idx])
+		{
+		    controle[idx] = goedenKleurEnPlekPrint;
+		    mainControle[idx] = true;
+		}
+
+		else
+		    for (int y = 0; y < codeVak.length; y = y + 1)
+		    {
+			if (poging[idx] != codeVak[idx] && poging[idx] == codeVak[y])
+			{
+			    controle[idx] = goedenKleurfoutenPlekPrint;
+			}
+		    }
+	    }
+	    return controle;
+    } 
+ public int spelerwinst() {
+//     winst controle
+	    boolean spelerWinst = true;
+	    for (int idx = 0; idx < mainControle.length; idx = idx + 1)
+	    {
+		if (mainControle[idx] == false)
+		{
+		    spelerWinst = false;
+		    System.out.println("je hebt niet alles goed");
+		    levens = levens - 1;
+		    System.out.println("je hebt " + levens + " levens over");
+		    break;
+		}
+
+	    }
+	    if (spelerWinst)
+	    {
+		System.out.println("je hebt alles goed je wint");
+		levens = -1;
+	    }
+	    return levens;
+ }
 }
+
--- src/masterMind/MasterMind.java
+++ src/masterMind/MasterMind.java
@@ -10,70 +10,76 @@ public class MasterMind
     {
 	// TODO Auto-generated method stub
 	Scanner sc = new Scanner(System.in);
-	Random num = new Random();
+//	Random num = new Random();
 //code maker
-	String goedenKleurfoutenPlekPrint = "0";
-	String goedenKleurEnPlekPrint = "x";
-	int codenGrooten = 4;
-//code vak
-	int[] codeVak = new int[codenGrooten];
-	for (int idx = 0; idx < codeVak.length; idx = idx + 1)
-	{
-	    codeVak[idx] = num.nextInt(6) + 1;
-	}
+//	String goedenKleurfoutenPlekPrint = "0";
+//	String goedenKleurEnPlekPrint = "x";
+//	int codenGrooten = 4;
+	Helper helper = new Helper();
+	//helper.createCode();
+	int[] codeVak = helper.createCode();
+////code vak
+//	int[] codeVak = new int[codenGrooten];
+//	for (int idx = 0; idx < codeVak.length; idx = idx + 1)
+//	{
+//	    codeVak[idx] = num.nextInt(6) + 1;
+//	}
 
 //	 test random code generator
 	for (int idx = 0; idx < codeVak.length; idx= idx +1) {
             System.out.print(codeVak[idx] + "");};
      System.out.println("");
-
-	int levens = 10;
+//
+     int levens = helper.levens;
 	do
 	{// start loop
 	 // pogingen
-	    int[] poging = new int[codenGrooten];
-	    for (int idx = 0; idx < poging.length; idx = idx + 1)
-	    {
-		System.out.println("code kraker kiest een cijfer uit 1 t/m 6");
-		poging[idx] = sc.nextInt();
-	    }
+//	    int[] poging = new int[codenGrooten];
+//	    for (int idx = 0; idx < poging.length; idx = idx + 1)
+//	    {
+//		System.out.println("code kraker kiest een cijfer uit 1 t/m 6");
+//		poging[idx] = sc.nextInt();
+//	    }
 
+	   
+	    // controle aan maak
+//	    String[] controle = new String[codenGrooten];
+//	    for (int idx = 0; idx < controle.length; idx = idx + 1)
+//	    {
+//		controle[idx] = "*";
+//	    }
+//
+//	    boolean[] mainControle = new boolean[codenGrooten];
+//	    for (int idx = 0; idx < controle.length; idx = idx + 1)
+//	    {
+//		mainControle[idx] = false;
+//	    }
+//	    // 1st contole checks
+//	    for (int idx = 0; idx < poging.length; idx = idx + 1)
+//	    {
+//		if (poging[idx] == codeVak[idx])
+//		{
+//		    controle[idx] = goedenKleurEnPlekPrint;
+//		    mainControle[idx] = true;
+//		}
+//
+//		else
+//		    for (int y = 0; y < codeVak.length; y = y + 1)
+//		    {
+//			if (poging[idx] != codeVak[idx] && poging[idx] == codeVak[y])
+//			{
+//			    controle[idx] = goedenKleurfoutenPlekPrint;
+//			}
+//		    }
+//	    }
+//	    boolean[] mainControle= helper.
+	    int[] poging = helper.pogingSpeler();
+	    String[] controle = helper.controlenCheck();
+	    // poging feedback
+	    System.out.println("feedback poging:");
 	    System.out.println("x is goed ");
 	    System.out.println("0 is verkeerde plek ");
 	    System.out.println("* zit er niet in");
-	    // controle aan maak
-	    String[] controle = new String[codenGrooten];
-	    for (int idx = 0; idx < controle.length; idx = idx + 1)
-	    {
-		controle[idx] = "*";
-	    }
-
-	    boolean[] mainControle = new boolean[codenGrooten];
-	    for (int idx = 0; idx < controle.length; idx = idx + 1)
-	    {
-		mainControle[idx] = false;
-	    }
-	    // 1st contole checks
-	    for (int idx = 0; idx < poging.length; idx = idx + 1)
-	    {
-		if (poging[idx] == codeVak[idx])
-		{
-		    controle[idx] = goedenKleurEnPlekPrint;
-		    mainControle[idx] = true;
-		}
-
-		else
-		    for (int y = 0; y < codeVak.length; y = y + 1)
-		    {
-			if (poging[idx] != codeVak[idx] && poging[idx] == codeVak[y])
-			{
-			    controle[idx] = goedenKleurfoutenPlekPrint;
-			}
-		    }
-	    }
-
-	    // poging feedback
-	    System.out.println("feedback poging:");
 	    for (int idx = 0; idx < poging.length; idx = idx + 1)
 	    {
 		System.out.print(poging[idx]);
@@ -84,25 +90,26 @@ public class MasterMind
 		System.out.print(controle[idx]);
 	    }
 	    System.out.println("");
-	    // winst controle
-	    boolean spelerWinst = true;
-	    for (int idx = 0; idx < mainControle.length; idx = idx + 1)
-	    {
-		if (mainControle[idx] == false)
-		{
-		    spelerWinst = false;
-		    System.out.println("je hebt niet alles goed");
-		    levens = levens - 1;
-		    System.out.println("je hebt " + levens + " levens over");
-		    break;
-		}
-
-	    }
-	    if (spelerWinst)
-	    {
-		System.out.println("je hebt alles goed je wint");
-		levens = -1;
-	    }
+//	     winst controle
+//	    boolean spelerWinst = true;
+//	    for (int idx = 0; idx < mainControle.length; idx = idx + 1)
+//	    {
+//		if (mainControle[idx] == false)
+//		{
+//		    spelerWinst = false;
+//		    System.out.println("je hebt niet alles goed");
+//		    levens = levens - 1;
+//		    System.out.println("je hebt " + levens + " levens over");
+//		    break;
+//		}
+//
+//	    }
+//	    if (spelerWinst)
+//	    {
+//		System.out.println("je hebt alles goed je wint");
+//		levens = -1;
+//	    }
+	     levens = helper.spelerwinst();
 	} while (levens > 0);// eind loop
 	if (levens == 0)
 	{
